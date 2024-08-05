c755b190867a6378947811800eb7b8d4681718d6
Angela Dube <angeladube874@gmail.com>
2024-08-05 20:41:23 +0200

    Added two functions and an element
    functions[searchCity(city) and searchSubmit(event)] with searchFormElement

---
+++ src/app.js
@@ -0,0 +1,16 @@
+function searchCity(city) {
+  let apiKey = "60o33fb20b2774044706tc81f87a3c87";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}";
+  axios.get(apiUrl).then(refresh);
+}
+function searchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchSubmit);
+
+searchCity("Paris");
